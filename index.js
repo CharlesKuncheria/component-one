3962c2cdf6e43c6ec84a71e72ee5455e7d9a8098
Charles Kuncheria <chkunche@microsoft.com>
2017-02-09 11:19:59 +0530

    add more logic

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-angular.module('ckComponentOne', []).
-  directive('ckDirectiveOne', function () {
-    // implementation goes here
-  });
+angular
+  .module('myApp', ['bar', 'baz'])
+  .config(function () { console.log('myApp config') })
+  .config(function(fnord) {console.log(fnord)})
+  .run(function () { console.log('myApp run') });
+  
+angular.module('myApp').filter('greet', function() {
+ return function(name) {
+    return 'Hello, ' + name + '!';
+  };
+});  
+
+
+// bar
+
+angular
+  .module('bar', ['baz'])
+  .config(function () { console.log('bar config') })
+  .config(function ($provide) {
+    $provide.constant('fnord', 5);
+  })
+  .run(function () { console.log('bar run') });
+  
+// baz  
+  
+  
+angular
+  .module('baz', [])
+  .config(function () { console.log('baz config') })
+  .run(function () { console.log('baz run') });
